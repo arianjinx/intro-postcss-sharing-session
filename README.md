9cef29b3482047d8dc724821ea38b46f69a763be
Arian Pradana <arian.raptor@gmail.com>
2016-10-04 14:32:58 +0700

    Ready to rumble

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Intro PostCSS sharing session
 Source code buat sharing session Product function Tokopedia
+
+- To Do result warn using postcss-reporter for doiuse
--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,9 @@ gulp.task('server', function (done) {
   browser.init({
     injectChanges: true,
     server: './',
+    port: 4000,
     open: false,
-    domain: 'http://localhost:3000',
+    domain: 'http://localhost:4000',
     ghostMode: {
       clicks: false,
       forms: false,
@@ -41,7 +42,6 @@ gulp.task('css', function () {
     require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
-    require('postcss-assets'),
     require('postcss-inline-svg'),
     require('postcss-svgo'),
     require('doiuse')({
@@ -52,28 +52,36 @@ gulp.task('css', function () {
         'Android 2.3',
         'iOS 7.1'
       ],
+      ignoreFiles: ['**/normalize.css'],
       onFeatureUsage: function (usageInfo) {
         console.log(usageInfo.message);
       }
     }),
     require('postcss-easysprites')({
       imagePath: './src/img/sprites',
-      spritePath: './dist/img',
+      spritePath: './src/img',
       stylesheetPath: './dist/css'
     }),
     require('postcss-at2x'),
     require('autoprefixer'),
+    require('postcss-reporter'),
     require('stylefmt')
   ];
 
   return gulp.src('src/css/**/*.css')
   .pipe($.postcss(CONFIG))
+  .pipe($.if(PRODUCTION, $.postcss([
+    require('postcss-url')({
+      url: 'rebase'
+    })], {to: './dist/css/folder/*.css'}
+  )))
   .pipe($.if(PRODUCTION, $.postcss([require('cssnano')])))
   .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('watch', function () {
   gulp.watch(['src/css/**/*.css']).on('change', gulp.series('css', browser.reload));
+  gulp.watch(['src/img/**/*.*']).on('change', gulp.series('images', browser.reload));
   gulp.watch(['*.html']).on('change', gulp.series(browser.reload));
 });
 
